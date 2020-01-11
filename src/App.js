986c612ef1e3a6db93bc4b6e1c36060aaa083f5e
Joshua Tu <tjh8523@gmail.com>
2020-01-11 18:42:38 +1100

    filtering is now functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFiled: ""
+    };
+  }
+
+  componentDidMount() {
+    try {
+      (async () => {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const users = await response.json();
+        this.setState({ monsters: users });
+      })();
+    } catch (err) {
+      throw "Error: " + err;
+    }
+  }
+
+  render() {
+    const { monsters, searchFiled } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search monsters"
+          onChange={e => {
+            this.setState({ searchFiled: e.target.value }, () =>
+              console.log(this.state)
+            );
+          }}
+          // value={this.state.searchFiled}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
